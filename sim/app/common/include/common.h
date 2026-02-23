e52b83f6ec45f61c633136c96769b0a5646b5bb3
Hamster5295 <hamster5295@163.com>
2026-02-23 15:21:49 +0800

    feat: LRU rule for btb

---
+++ sim/app/common/include/common.h
@@ -0,0 +1,6 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+void stop(int ret);
+
+#endif // __COMMON_H
---
+++ sim/app/common/src/common.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+void stop(int ret) {
+  asm("ebreak");
+  while (1) {
+  }
+}
