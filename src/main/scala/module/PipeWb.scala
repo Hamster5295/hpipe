84b1e25081efbba60e15e2ecb64a64d3459c574d
Hamster5295 <hamster5295@163.com>
2026-03-27 01:01:39 +0800

    feat: izcsr & rename CPU -> HPipe

--- src/main/scala/module/PipeWb.scala
+++ src/main/scala/module/PipeWb.scala
@@ -2,10 +2,14 @@ package hpipe
 
 import chisel3._
 import chisel3.util._
+import hammer._
 
 class PipeWbIO(implicit p: Parameters) extends StageIO {
   val fromMem  = Input(new Mem2WbIO)
   val regWrite = Flipped(new RegFileWritePort)
+
+  val csrRead  = Flipped(new CsrReadPort)
+  val csrWrite = Flipped(new CsrWritePort)
 
   val retire = Output(new RetireInfo)
 }
@@ -16,11 +20,22 @@ class PipeWb(implicit val p: Parameters) extends Module {
 
   io.busy := false.B
 
+  io.csrRead.addr := fromMem.csr
+  val dest = Mux(fromMem.isCsr, io.csrRead.data, fromMem.data)
+
   io.regWrite.addr := Mux(fromMem.writeRd, fromMem.rd, 0.U)
-  io.regWrite.data := fromMem.data
+  io.regWrite.data := dest
+
+  val csrDest = Mux(
+    fromMem.csrOp === "b010".U,
+    fromMem.data | io.csrRead.data,
+    fromMem.data,
+  )
+  io.csrWrite.addr := fromMem.csr
+  io.csrWrite.data := csrDest
 
   val retire = io.retire
   retire.valid  := fromMem.valid
   retire.pc     := fromMem.pc
-  retire.ebreak := fromMem.ebreak
+  retire.ebreak := fromMem.isEBreak
 }
