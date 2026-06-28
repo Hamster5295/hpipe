c1aa1e51a7b38377c3b0139440eca508a2863e6d
Hamster5295 <hamster5295@163.com>
2026-06-28 21:30:43 +0800

    feat: set target to 300MHz

--- backend/fpga/script/synth.tcl
+++ backend/fpga/script/synth.tcl
@@ -1,6 +1,6 @@
 open_project hpipe.xpr
 
-synth_design -top HPipe
+synth_design -top HPipe -directive PerformanceOptimized -retiming -flatten_hierarchy full
 
 report_timing -sort_by group -max_paths 256 -path_type summary -file ../report/timing.summary.rpt
 report_timing -sort_by group -nworst 20 -file ../report/timing.rpt
