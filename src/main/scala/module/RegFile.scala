60a23c1b0f8e06a9da6c649e4a095438de45149a
Hamster5295 <hamster5295@163.com>
2026-06-29 19:57:28 +0800

    refactor: rebuild RegFile for better timing

--- src/main/scala/module/RegFile.scala
+++ src/main/scala/module/RegFile.scala
@@ -26,25 +26,34 @@ class RegFile(implicit val p: HPipeParameters) extends Module {
   val regs = RegZero(Vec(p.XLEN - 1, Word()))
 
   io.regs := regs
-  io.read.map(i => i.data := 0.U)
 
-  val passthroughs = io.read.map(i => (i, i.addr === io.write.addr)).toMap
-
-  io.read.map { i =>
-    when(i.addr === 0.U) {
-      i.data := 0.U
-    }
-  }
+  io.read.map(r => r.data := 0.U)
 
   regs.zipWithIndex.map { case (reg, idx) =>
     val addr = (idx + 1).U
-    io.read.map { i =>
-      when(i.addr === addr) {
-        i.data := Mux(passthroughs(i), io.write.data, reg)
-      }
-    }
-    when(io.write.addr === addr) {
-      reg := io.write.data
-    }
+    io.read.map(r => when(r.addr === addr)(r.data := reg))
+    reg := Mux(io.write.addr === addr, io.write.data, reg)
   }
+
+  io.read.map(r => when(r.addr === io.write.addr)(r.data := io.write.data))
+  io.read.map(r => when(!r.addr.orR)(r.data := 0.U))
+
+//   io.read.map(i => i.data := 0.U)
+//   io.read.map { i =>
+//     when(i.addr === 0.U) {
+//       i.data := 0.U
+//     }
+//   }
+
+//   regs.zipWithIndex.map { case (reg, idx) =>
+//     val addr = (idx + 1).U
+//     io.read.map { i =>
+//       when(i.addr === addr) {
+//         i.data := Mux(passthroughs(i), io.write.data, reg)
+//       }
+//     }
+//     when(io.write.addr === addr) {
+//       reg := io.write.data
+//     }
+//   }
 }
