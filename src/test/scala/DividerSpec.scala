91a6af08a54f64d0e5c3a670391ebbe4e9917c63
Hamster5295 <hamster5295@163.com>
2026-08-16 16:35:42 +0800

    fix: Divider unit test

--- src/test/scala/DividerSpec.scala
+++ src/test/scala/DividerSpec.scala
@@ -11,64 +11,67 @@ import org.scalatest.matchers.must.Matchers
 
 class DividerSpec extends AnyFreeSpec with Matchers with ChiselSim {
 
-  def sim(width: Int) = Sim(new IntNonRestoringDiv(width, false)) { dut =>
-    Test(s"UInt$width", dut) { dut =>
-      dut.io.signed.poke(false)
-      for (i <- 0 until 1024) {
-        val a = Fixed.uint(width)
-        val b = Fixed.uint(width).max(BigInt(1))
-        // val a   = BigInt(10)
-        // val b   = BigInt(3)
-        val quo = a / b
-        val rem = a % b
+  def sim(width: Int) =
+    Sim(new IntNonRestoringDiv(width)(HPipeParameters())) { dut =>
+      Test(s"UInt$width", dut) { dut =>
+        dut.io.signed.poke(false)
+        for (i <- 0 until 1024) {
+          val a   = Fixed.uint(width)
+          val b   = Fixed.uint(width).max(BigInt(1))
+          val quo = a / b
+          val rem = a % b
 
-        dut.io.valid.poke(true)
-        dut.io.dividend.poke(a)
-        dut.io.divisor.poke(b)
-        dut.clock.step()
-        dut.io.valid.poke(false)
-
-        while (dut.io.busy.peekBoolean()) {
+          dut.io.valid.poke(true)
+          dut.io.dividend.poke(a)
+          dut.io.divisor.poke(b)
           dut.clock.step()
-        }
+          dut.io.valid.poke(false)
 
-        dut.io.quotient.expect(quo)
-        dut.io.remainder.expect(rem)
+          while (dut.io.busy.peekBoolean()) {
+            dut.clock.step()
+          }
+
+          dut.clock.step()
+
+          dut.io.quotient.expect(quo)
+          dut.io.remainder.expect(rem)
+        }
+      }
+      Test(s"SInt$width", dut) { dut =>
+        dut.io.signed.poke(true)
+        for (i <- 0 until 1024) {
+          val a     = Fixed.sint(width)
+          val btemp = Fixed.sint(width)
+
+          val b = if (btemp == 0) BigInt(1) else btemp
+
+          val quo = a / b
+          val rem = a % b
+
+          dut.io.valid.poke(true)
+          dut.io.dividend.poke(a)
+          dut.io.divisor.poke(b)
+          dut.clock.step()
+          dut.io.valid.poke(false)
+
+          while (dut.io.busy.peekBoolean()) {
+            dut.clock.step()
+          }
+
+          dut.clock.step()
+
+          Expect(
+            dut.io.quotient,
+            quo & Fixed.mask(width),
+          )(ob => ob & Fixed.mask(width))
+
+          Expect(
+            dut.io.remainder,
+            rem & Fixed.mask(width),
+          )(ob => ob & Fixed.mask(width))
+        }
       }
     }
-    Test(s"SInt$width", dut) { dut =>
-      dut.io.signed.poke(true)
-      for (i <- 0 until 1024) {
-        val a     = Fixed.sint(width)
-        val btemp = Fixed.sint(width)
-
-        val b = if (btemp == 0) BigInt(1) else btemp
-
-        val quo = a / b
-        val rem = a % b
-
-        dut.io.valid.poke(true)
-        dut.io.dividend.poke(a)
-        dut.io.divisor.poke(b)
-        dut.clock.step()
-        dut.io.valid.poke(false)
-
-        while (dut.io.busy.peekBoolean()) {
-          dut.clock.step()
-        }
-
-        Expect(
-          dut.io.quotient,
-          quo & Fixed.mask(width),
-        )(ob => ob & Fixed.mask(width))
-
-        Expect(
-          dut.io.remainder,
-          rem & Fixed.mask(width),
-        )(ob => ob & Fixed.mask(width))
-      }
-    }
-  }
 
   "Divider should divide correctly" in sim(32)
 
